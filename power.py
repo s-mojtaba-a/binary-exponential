862baab8edc21a9511ef516ecaf7f06768a01e2a
Mojtaba Abedi <68202630+s-mojtaba-a@users.noreply.github.com>
2020-09-07 20:59:28 +0430

    source code

---
+++ power.py
@@ -0,0 +1,20 @@
+def power(x,y,n):
+    ans=1
+    while y>0 :
+        if y&1==1 :
+            ans=ans*x%n
+        x=x*x%n
+        y>>=1
+    return(ans)
+
+
+def power(x,y):
+    ans=1
+    while y>0 :
+        if y%2==1 :
+            ans*=x
+        x*=x
+        y>>=1
+    return(ans)
+
+
